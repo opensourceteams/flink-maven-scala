d0f899174d1e5d9c4bafe6085f9bad5d24cd844f
thinktothings <thinktothings@gmail.com>
2019-03-08 18:47:59 +0800

    指定时间戳

---
+++ src/main/scala/com/opensourceteams/module/bigdata/flink/example/datastream/operator/assignTimestamps/Run.scala
@@ -0,0 +1,54 @@
+package com.opensourceteams.module.bigdata.flink.example.datastream.operator.assignTimestamps
+
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.streaming.api.windowing.time.Time
+
+/**
+  * nc -lk 1234  输入数据
+  */
+object Run {
+
+  def main(args: Array[String]): Unit = {
+
+
+    val port = 1234
+    // get the execution environment
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)  //设置并行度
+    val dataStream = env.socketTextStream("localhost", port, '\n')
+
+    dataStream.assignAscendingTimestamps(x => System.currentTimeMillis())
+
+    val dataStream2 = dataStream.flatMap(x => x.split(" ")).map((_,1))
+      .keyBy(0)
+
+
+      .timeWindow(Time.seconds(2))//每2秒滚动窗口
+      .sum(1)
+
+
+
+
+
+    dataStream2.print()
+
+
+
+
+    println("=======================打印StreamPlanAsJSON=======================\n")
+    println("JSON转图在线工具: https://flink.apache.org/visualizer")
+    println(env.getStreamGraph.getStreamingPlanAsJSON)
+    println("==================================================================\n")
+
+    if(args == null || args.size ==0){
+      env.execute("默认作业")
+    }else{
+      env.execute(args(0))
+    }
+
+    println("结束")
+
+  }
+
+
+}
